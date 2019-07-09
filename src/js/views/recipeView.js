fe601131cd7a0bbfa38155d175ac0ea81f1dd33f
Christian Agha <christian@Christians-iMac.fios-router.home>
2019-07-09 16:45:16 -0400

    finishing adding list functionality

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -75,7 +75,7 @@ export const renderRecipe = recipe => {
                 ${recipe.ingredients.map(el => createIngredient(el)).join('')}
             </ul>
 
-            <button class="btn-small recipe__btn">
+            <button class="btn-small recipe__btn recipe__btn--add">
                 <svg class="search__icon">
                     <use href="img/icons.svg#icon-shopping-cart"></use>
                 </svg>
@@ -98,7 +98,7 @@ export const renderRecipe = recipe => {
             </a>
         </div>
     `;
-    elements.recipe.insertAdjacentHTML('afterbegin', markup)
+    elements.recipe.insertAdjacentHTML('afterbegin', markup);
 };
 
 export const updateServingsIngredients = recipe => {
